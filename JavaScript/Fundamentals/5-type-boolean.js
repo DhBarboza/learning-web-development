ea499a0cd1b97a5614685ceed6bd2cb38ff873cd
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-11-16 22:28:54 -0300

    Add Type Boolean

---
+++ JavaScript/Fundamentals/5-type-boolean.js
@@ -0,0 +1,11 @@
+// Definindo valores falso e verdadeiro:
+let perigo = false;
+let certeza = true;
+
+console.log(`O Perigo é verdadeiro: ${perigo}, A certeza é: ${certeza}`);
+
+// Convertendo valor numérico para verdadeiro ou falso:
+let value = 1;
+console.log("Duas exclamações:", !!value);
+console.log("Uma exclamação:", !value);
+
