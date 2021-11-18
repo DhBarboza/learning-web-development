fe8ba1acaca24f190a8cbbc5accc82aaf1033587
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-11-17 21:36:37 -0300

    Add object exemplos

---
+++ JavaScript/Fundamentals/7-Object.js
@@ -0,0 +1,13 @@
+// Criar um objeto:
+const produto = {};
+
+// Adicionar uma chave e valor:
+produto.nome = "Poco X3 NFC";
+produto.preço = 1300.0;
+
+console.log(produto);
+
+const mercadoria = {
+  tipo: "Eletrônico",
+  preco: 4500.0,
+};
